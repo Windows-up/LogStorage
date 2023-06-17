2ecbb4715f0cca6dc222e2d8a23f847673325481
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-06-17 15:16:44 +0300

    Init

---
+++ src/main/java/ru/doctorixx/applogstorage/AppLogStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.doctorixx.applogstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLogStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppLogStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/doctorixx/applogstorage/AppLogStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.doctorixx.applogstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLogStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
